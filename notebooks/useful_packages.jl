d77291d4135384912524ca549aa56bb9e75fa6cc
Phillip Alday <me@phillipalday.com>
2021-09-02 04:53:14 +0000

    notebooks

--- notebooks/useful_packages.jl
+++ notebooks/useful_packages.jl
@@ -4,6 +4,12 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 539bb62b-e4e3-4d72-837c-302ac9335e2e
+begin
+	using PlutoUI
+	TableOfContents()
+end
+
 # ╔═╡ 27130015-4f45-4ebd-9094-07ec00261e5f
 md"""
 # Useful Packages
@@ -140,10 +146,109 @@ For MATLAB users, there is also [MATLAB.jl](https://github.com/JuliaInterop/MATL
 Support for calling C and Fortran is [part of the Julia standard library](https://docs.julialang.org/en/v1/manual/calling-c-and-fortran-code/).
 """
 
+# ╔═╡ 35c0e59d-23ce-4b60-b80c-e619a50f990d
+md"""
+
+## Notebooks and Webpages
+
+We haved [Pluto.jl](https://github.com/fonsp/Pluto.jl/) throughout this course because it provides a nice, easy way to interact with Julia, including automatic package management. [PlutoUI.jl](https://github.com/fonsp/PlutoUI.jl) provides some nice tools for interacting with Pluto notebooks and making them more dynamic (including the `TableOfContents()` in this one!). We also like that Pluto notebooks are essentially normal Julia code stored in plain text under the hood, which means that they play quite nice with version control systems such as Git. Sometimes, however, the reactive nature of Pluto (update any one cell and the changes propogate to all impacted cells) is not desireable.
+
+An alternative notebook interface is provide by [Jupyter](https://jupyter.org/), formerly IPython. Indeed, the "Ju" actually stands for Julia. Jupyter support is provided by the package [IJulia.jl](https://julialang.github.io/IJulia.jl/stable/).
+
+The [Weave.jl](https://weavejl.mpastell.com/stable/) package provides "Julia markdown", which is similar to knitr/RMarkdown or the the Sweave formats in R. Notably, Weave also provides support for converting between `jmd` files and Jupyter notebooks.
+
+Similarly, [Literate.jl](https://fredrikekre.github.io/Literate.jl/v2/) is a simple package for literate programming (i.e. programming where documentation and code are "woven" together) and can generate Markdown, plain code and Jupyter notebook output. It is designed for simplicity and speed and is less extensive than Weave.jl and Documenter.jl.
+
+[Documenter.jl](https://juliadocs.github.io/Documenter.jl/stable/) is the standard tool for building webpages from Julia documentation
+
+[Books.jl](https://rikhuijzer.github.io/Books.jl/) is a package designed to offer somewhat similar functionality to the `bookdown` package in R.
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+
+[compat]
+PlutoUI = "~0.7.9"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+[[Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "8076680b162ada2a031f707ac7b4953e30667a37"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.2"
+
+[[Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[Parsers]]
+deps = ["Dates"]
+git-tree-sha1 = "438d35d2d95ae2c5e8780b330592b6de8494e779"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.0.3"
+
+[[PlutoUI]]
+deps = ["Base64", "Dates", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "Suppressor"]
+git-tree-sha1 = "44e225d5837e2a2345e69a1d1e01ac2443ff9fcb"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.9"
+
+[[Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[Random]]
+deps = ["Serialization"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[Suppressor]]
+git-tree-sha1 = "a819d77f31f83e5792a76081eee1ea6342ab8787"
+uuid = "fd094767-a336-5f1f-9728-57cf17d0bbfb"
+version = "0.2.0"
+
+[[Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+"""
+
 # ╔═╡ Cell order:
+# ╟─539bb62b-e4e3-4d72-837c-302ac9335e2e
 # ╟─27130015-4f45-4ebd-9094-07ec00261e5f
 # ╟─cd73a70f-4ce0-4ef8-87be-d3b319066602
 # ╟─9143e623-5457-48b5-9304-ce2a757b18b5
 # ╟─32c08c8a-68ec-4bcb-bd4b-3fd76ffca0f0
 # ╟─4df36c00-be80-4fa5-83ca-c9648352fc42
 # ╟─919a3d3c-1375-46ab-8507-ae884bb52cfb
+# ╟─35c0e59d-23ce-4b60-b80c-e619a50f990d
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
